bf032410667cba534754e24b3c4111adb0cae264
311609 <94932711+311609@users.noreply.github.com>
2023-06-18 01:50:43 -0600

    se hace autenticacion con express

--- app.js
+++ app.js
@@ -1,35 +1,30 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
 const app = express();
 const listViewRouter = require('./list-review-router');
 const listEditRouter = require('./list-edit-router');
 
-app.use(express.json()); // Middleware para procesar el cuerpo de las solicitudes JSON
-app.use(express.urlencoded({ extended: true })); // Middleware para procesar el cuerpo de las solicitudes URL-encoded
+dotenv.config(); // Cargar variables de entorno desde el archivo .env
 
-// Middleware a nivel de aplicación para gestionar métodos HTTP válidos
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use((req, res, next) => {
   if (!['GET', 'POST', 'PUT', 'DELETE'].includes(req.method)) {
     return res.status(400).json({ error: 'Método HTTP no válido' });
   }
-
   next();
 });
 
-// Middleware para gestionar parámetros correctos en el direccionador list-view-router
 app.use('/list-view', (req, res, next) => {
-  // Verificar si los parámetros son correctos (por ejemplo, si existen y tienen el formato adecuado)
-  // Si los parámetros no son correctos, se puede devolver un código de respuesta 400 y un mensaje de error
-  // En caso contrario, llamar a next() para pasar al siguiente middleware o enrutador
   next();
 });
 
-// Rutas para listar tareas completas e incompletas
 app.use('/list-view', listViewRouter);
 
-// Rutas para crear, eliminar y actualizar tareas
 app.use('/list-edit', listEditRouter);
 
-// Ruta para obtener la lista de tareas
 app.get('/tasks', (req, res) => {
   console.log('Solicitud GET recibida en la ruta /tasks');
 
@@ -44,6 +39,43 @@ app.get('/tasks', (req, res) => {
   tasks[0].description = 'Walk the cat';
 
   res.json(tasks);
+});
+
+// Ruta de autenticación (inicio de sesión)
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Verificar las credenciales del usuario
+  if (username === 'admin' && password === 'password') {
+    // Generar el token JWT
+    const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token });
+  } else {
+    res.status(401).json({ error: 'Credenciales inválidas' });
+  }
+});
+
+// Middleware para validar el token JWT
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token == null) {
+    return res.status(401).json({ error: 'Token de autenticación no proporcionado' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: 'Token de autenticación inválido' });
+    }
+    req.user = user;
+    next();
+  });
+};
+
+// Ruta protegida que requiere autenticación
+app.get('/protected-route', authenticateToken, (req, res) => {
+  res.json({ message: 'Ruta protegida accesada con éxito' });
 });
 
 const port = 3000;
