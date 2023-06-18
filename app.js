7a6dc84886be29fb1356cd603c3eb2995b16d1e5
311609 <94932711+311609@users.noreply.github.com>
2023-06-17 19:05:46 -0600

    se agregan rutas para manejo de tareas

--- app.js
+++ app.js
@@ -1,49 +1,16 @@
 const express = require('express');
 const app = express();
-
+const listViewRouter = require('./list-view-router');
+const listEditRouter = require('./list-edit-router');
 
 app.use(express.json()); // Middleware para procesar el cuerpo de las solicitudes JSON
 app.use(express.urlencoded({ extended: true })); // Middleware para procesar el cuerpo de las solicitudes URL-encoded
 
-// Middleware para gestionar solicitudes por métodos HTTP válidos
-app.use((req, res, next) => {
-  if (req.method !== 'GET' && req.method !== 'POST' && req.method !== 'PUT') {
-    return res.status(400).json({ error: 'Método HTTP no válido' });
-  }
-  next();
-});
+// Rutas para listar tareas completas e incompletas
+app.use('/list-view', listViewRouter);
 
-
-// Middleware para el router list-edit-router
-const listEditMiddleware = (req, res, next) => {
-  if (req.method === 'POST') {
-    if (!req.body || Object.keys(req.body).length === 0) {
-      return res.status(400).json({ error: 'Cuerpo de la solicitud vacío' });
-    }
-  
-    // Si hay información no válida o atributos faltantes, retorna un código de respuesta 400
-  }
-
-  if (req.method === 'PUT') {
-    if (!req.body || Object.keys(req.body).length === 0) {
-      return res.status(400).json({ error: 'Cuerpo de la solicitud vacío' });
-    }
-  
-    // Si hay información no válida o atributos faltantes, retorna un código de respuesta 400
-  }
-
-  next();
-};
-
-// Middleware para el direccionador list-view-router
-const listViewMiddleware = (req, res, next) => {
-
-  // Si los parámetros no son correctos, retorna un código de respuesta 400
-  next();
-};
-
-app.use('/list-edit', listEditMiddleware);
-app.use('/list-view', listViewMiddleware);
+// Rutas para crear, eliminar y actualizar tareas
+app.use('/list-edit', listEditRouter);
 
 // Ruta para obtener la lista de tareas
 app.get('/tasks', (req, res) => {
